1a48141c9c03854904f1aa5554c523412ae6eb43
HirotoYano <mi22a012@oecu.jp>
2023-09-20 17:04:13 +0900

    Revised to turn the study around for a number of meeting minutes.

--- src/1_sbert_fine_tuning_for_assembly_triple.py
+++ src/1_sbert_fine_tuning_for_assembly_triple.py
@@ -1,4 +1,8 @@
 # # %%
+import os
+from datetime import datetime
+from typing import List
+
 import hydra
 
 # import os
@@ -20,7 +24,8 @@ def main(
     batch_size: int = 256,
     use_dirver: str = "cuda:0",
     num_epochs: int = 10,
-    evaluation_steps: int = 500,
+    # evaluation_steps: int = 500,
+    evaluation_steps: int = 100,
     loss_finction: str = "MultipleNegativesRankingLoss",
     evaluat_function: str = "TripletEvaluator",
     output_path: str = "/workspace/src",
@@ -28,12 +33,14 @@ def main(
     dataloader = Assembly_Triple_Dataloader(dataset_file_path)
     dataset = dataloader.get_examples(dataset_file_name)
 
+    save_path = f"{output_path}/{dense_dim}_{os.path.splitext(os.path.basename(dataset_file_name))[0]}"
+
     sbert_model = Sentence_BERT_Model(
         model_name=model_name,
         max_seq_length=max_seq_length,
         use_dirver=use_dirver,
         dense_dim=dense_dim,
-        model_save_file_path=output_path,
+        model_save_file_path=save_path,
     )
 
     sbert_model.show_settings()
@@ -52,18 +59,27 @@ def main(
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def my_app(cfg: DictConfig):
-    main(
-        dataset_file_path=cfg.model.dataset_file_path,
-        dataset_file_name=cfg.model.dataset_file_name,
-        dense_dim=cfg.model.dense_dim,
-        model_name=cfg.model.model_name,
-        max_seq_length=cfg.model.max_seq_length,
-        use_dirver=cfg.model.use_dirver,
-        num_epochs=cfg.model.num_epochs,
-        evaluation_steps=cfg.model.evaluation_steps,
-        loss_finction=cfg.model.loss_finction,
-        evaluat_function=cfg.model.evaluat_function,
-    )
+    # file_name_path: List[str] = os.listdir("/workspace/src/outputs/compair_proposed_method")
+    # file_name_path: List[str] = ["一般質問(要旨)2月13日", "一般質問(要旨)12月9日", "一般質問(要旨)6月3日", "一般質問（要旨）2月26日", "一般質問(要旨)9月10日"]
+    file_name_path: List[str] = ["一般質問(要旨)2月13日"]
+    date_time = datetime.now().strftime("%Y-%m-%d/%H-%M-%S")
+
+    for file_name in file_name_path:
+        print(f"===== {file_name} =====")
+        main(
+            dataset_file_path=cfg.model.dataset_file_path,
+            # dataset_file_name=cfg.model.dataset_file_name,
+            dataset_file_name=f"{file_name}.csv",
+            dense_dim=cfg.model.dense_dim,
+            model_name=cfg.model.model_name,
+            max_seq_length=cfg.model.max_seq_length,
+            use_dirver=cfg.model.use_dirver,
+            num_epochs=cfg.model.num_epochs,
+            evaluation_steps=cfg.model.evaluation_steps,
+            loss_finction=cfg.model.loss_finction,
+            evaluat_function=cfg.model.evaluat_function,
+            output_path=f"/workspace/src/log/sentence_bert/{date_time}",
+        )
 
 
 if __name__ == "__main__":
